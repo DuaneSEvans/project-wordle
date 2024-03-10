a94fe072c6cb05875ecbf11af78fce3d55b319fb
Duane Evans <duanesevans@gmail.com>
2024-03-10 09:11:24 -0700

    My version of ex 1

---
+++ src/components/Input/Input.js
@@ -0,0 +1,32 @@
+import React from "react"
+
+function Input() {
+  const [guess, setGuess] = React.useState("")
+
+  const handleInput = (e) => {
+    setGuess(e.target.value.toUpperCase())
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.info({ guess })
+    setGuess("")
+  }
+
+  return (
+    <form className="guess-input-wrapper" onSubmit={handleSubmit}>
+      <label htmlFor="guess-input">Enter guess:</label>
+      <input
+        id="guess-input"
+        type="text"
+        value={guess}
+        pattern="\w{5,5}"
+        onChange={handleInput}
+        required
+        title="Please enter a 5-letter word."
+      />
+    </form>
+  )
+}
+
+export default Input
