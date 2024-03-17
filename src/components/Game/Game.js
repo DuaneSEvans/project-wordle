7679603fa6eabd99426218af7998777ed5449db7
Duane Evans <duanesevans@gmail.com>
2024-03-17 13:49:00 -0700

    use const

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -5,6 +5,7 @@ import { WORDS } from "../../data"
 import Input from "../Input"
 import PreviousGuesses from "../PreviousGuesses/PreviousGuesses"
 import EndGameBanner from "../EndGameBanner/EndGameBanner"
+import { NUM_OF_GUESSES_ALLOWED } from "../../constants"
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS)
@@ -14,7 +15,7 @@ console.info({ answer })
 function Game() {
   const [prevGuesses, setPrevGuesses] = React.useState([])
   const correctGuess = prevGuesses.includes(answer)
-  const gameOver = prevGuesses.length >= 6 || correctGuess
+  const gameOver = prevGuesses.length >= NUM_OF_GUESSES_ALLOWED || correctGuess
 
   return (
     <>
